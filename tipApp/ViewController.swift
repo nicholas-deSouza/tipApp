676c90437c9c38def26d9161fac833ad0b10afd5
Nicholas de Souza <nrdesouzag13@gmail.com>
2020-09-13 14:26:29 -0400

    Initial Commit

---
+++ tipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipApp
+//
+//  Created by Nicholas de Souza on 9/13/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
